37db8d6db588368e09d53dab10bbf65fbc823e61
David Ellis <d.f.ellis@ieee.org>
2013-06-04 09:15:09 -0700

    Adding code coverage and coveralls support.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
-var redisManager = require('../lib/redis-manager');
+var jscoverage = require('jscoverage');
+jscoverage.enableCoverage(true);
+var coveralls = require('coveralls');
+var redisManager = jscoverage.require(module, '../lib/redis-manager');
 
 exports.sameClientIsSame = function(test) {
     test.expect(1);
@@ -29,5 +32,39 @@ exports.freeClients = function(test) {
     var client2 = redisManager.getClient();
     test.equal(true, redisManager.freeClient(client1));
     test.equal(true, redisManager.freeClient(client2));
+    test.done();
+};
+
+exports.jscoverage = function(test) {
+    test.expect(1);
+    jscoverage.coverageDetail();
+    // Copied directly from jscoverage and edited, since getting at these values directly isn't possible
+    var file;
+    var tmp;
+    var total;
+    var touched;
+    var n, len;
+    if (typeof global._$jscoverage === 'undefined') {
+        return;
+    }
+    var lcov = "";
+    Object.keys(global._$jscoverage).forEach(function(key) {
+        file = key;
+        lcov += "SF:" + file + "\n";
+        tmp = global._$jscoverage[key];
+        if (typeof tmp === 'function' || tmp.length === undefined) return;
+        total = touched = 0;
+        for (n = 0, len = tmp.length; n < len; n++) {
+            if (tmp[n] !== undefined) {
+                lcov += "DA:" + n + "," + tmp[n] + "\n";
+                total ++;
+                if (tmp[n] > 0)
+                    touched ++;
+            }
+        }
+        test.equal(total, touched, 'All lines of code exercised by the tests');
+    });
+    lcov += "end_of_record\n";
+    if(process.env.TRAVIS) coveralls.handleInput(lcov);
     test.done();
 };
